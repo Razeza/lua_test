5e81eba7cb329cdb0e4c14e2ea1ca1c76d40aa9c
Danila <54887530+Razeza@users.noreply.github.com>
2021-05-17 13:18:28 +0300

    Create backtrace.c

---
+++ backtrace.c
@@ -0,0 +1,27 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <stdio.h>
+
+static int backtrace(lua_State* L)
+{
+    lua_Debug entry;
+    int depth = 0; 
+
+    while (lua_getstack(L, depth, &entry))
+    {
+        int status = lua_getinfo(L, "Sln", &entry);
+        assert(status);
+
+        printf("%s(%d): %s\n", entry.short_src, entry.currentline, entry.name ? entry.name : "?");
+        depth++;
+    }
+    
+    return 0;
+}
+
+int luaopen_mylib (lua_State *L){
+    lua_register(L, "backtrace", backtrace);
+    return 0;
+}
